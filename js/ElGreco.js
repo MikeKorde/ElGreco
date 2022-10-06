fb0b3a91802aa42de9c76f5409cea0f6c3b216f5
MikeKorde <mix.kordellas@gmail.com>
2022-10-06 11:51:43 +0200

    n

--- js/ElGreco.js
+++ js/ElGreco.js
@@ -5,7 +5,7 @@ let mybutton = document.getElementById("Btn");
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 40 || document.documentElement.scrollTop > 40) {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
     mybutton.style.display = "block";
   } else {
     mybutton.style.display = "none";
